f86ef40e867d4566053ca958aa1c110dc2afe994
Isaac Kim <kisaac95@yahoo.com>
2022-09-25 16:11:23 -0700

    v.9

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
+move(character).withArrowKeys(100,250, handleDirectionChange)
 
-setInterval(function() {
-    if (direction === 'west'){
-    x = x - 1
-}
-    if (direction === 'east'){
-    x = x + 1
-}
-    if (direction === 'north'){
-    y = y + 1
-}
-    if (direction === 'south'){
-    y = y - 1
-}
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
-console.log('fuck')
-}, 1)
 
 // setInterval(moveCharacter, 1)
 
@@ -39,23 +20,20 @@ move(newItem('assets/staff.png')).to(600, 250)
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
+function handleDirectionChange(direction){
+if(direction === null){
+    character.src='assets/green-character/static.gif'
+}
+if(direction === 'east'){
+    character.src='assets/green-character/east.gif'
+}
+if(direction === 'west'){
+    character.src='assets/green-character/west.gif'
+}
+if(direction === 'north'){
+    character.src='assets/green-character/north.gif'
+}
+if(direction === 'south'){
+    character.src='assets/green-character/south.gif'
+}
+}
